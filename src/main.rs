5673bd5aa841bcaca6e69274d36c6af2206ead73
Jul Lang <jullanggit@proton.me>
2025-01-23 09:41:06 +0100

    json -> serde + serde_json & finish initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
-use std::{env, fmt::format, fs, path::Path};
+use std::{collections::HashMap, env, fs, path::Path};
 
-use json::JsonValue;
+use serde::Deserialize;
+
+// The contents of the .cargo/.crates2.json file
+#[derive(Deserialize)]
+struct Crates2Json {
+    installs: HashMap<String, Install>,
+}
+// Maybe this can be inlined
+#[derive(Deserialize)]
+struct Install {
+    features: Vec<String>,
+    no_default_features: bool,
+}
 
 fn main() {
     let home = env::var("HOME").expect("$HOME env variable not set");
@@ -8,19 +20,40 @@ fn main() {
     let path = Path::new(&home).join(".cargo/.crates2.json");
     let string = fs::read_to_string(path).expect("Failed to read .crates2.json");
 
-    let json = json::parse(&string).expect("Failed to parse json");
+    let json: Crates2Json = serde_json::from_str(&string).expect("Failed to deserialize json");
 
-    let mut command = String::new();
+    let mut items = String::new();
 
-    if let JsonValue::Object(map) = &json["installs"] {
-        for (name, value) in map.iter() {
-            if let JsonValue::Array(features) = &value["features"] {
-                if !features.is_empty() {
-                    command.push_str("-F");
-                }
-            }
+    for (name, install) in json.installs {
+        if install.no_default_features {
+            items.push_str("--no-default-features");
+            items.push(' ');
         }
-    } else {
-        panic!()
+
+        if !install.features.is_empty() {
+            items.push_str("-F ");
+        }
+        for feature in install.features {
+            items.push_str(&feature);
+            items.push(' ');
+        }
+
+        let parts: Vec<_> = name.split_whitespace().collect();
+
+        let name = parts[0];
+        // Filter out parentheses
+        let url = parts[2];
+        let (source, url) = url.split_once('+').unwrap();
+        if source == "(git" {
+            items.push_str("--git ");
+
+            let url = url.split_once('#').unwrap().0;
+            items.push_str(url);
+            items.push(' ');
+        }
+        items.push_str(name);
+
+        items.push('\n');
     }
+    println!("{items}");
 }
